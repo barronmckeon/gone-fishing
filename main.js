97aa3bab81d2f6f718f22cb130b355da94ee205b
1 <a1@1s-MacBook-Pro.local>
2022-03-03 08:16:08 -0500

    rev2

--- main.js
+++ main.js
@@ -1,64 +1,70 @@
 
-const prompt = require('prompt-sync')()
-let i = 0;
+const prompt = require('prompt-sync')({sigint:true});
 let fishAdj1 = ['Slippery', 'Scaly', 'Luminescent', 'Shiny', 'Delicious', 'Crispy', 'Golden', 'Juicy', 'Battered', 'Smelly']
 let fishAdj2 = ['Fresh-Water','Great', 'Splendid', 'Lost', 'Straight ', 'Small', 'Fresh', 'First','Beautiful', 'Dead']
 let fishKind = ['Betta', 'Goldfish', 'Catfish', 'Swordfish', 'Tigershark', 'Ocean-Sunfish', 'Kaluga', 'Tuna', 'Mahi-Mahi', 'Sturgeon']
-// let to_Fish= prompt('Choose: [k]eep or [r]elease?')
+//let emptyBucket = [ ]
+let currentBucket = [ ]
+let currentBucketWeight = []
+let hour = 0
+//let currentWeight = 0
 
-let time = 0600 
-// let currentFish = randomFish()
-let fishValue = Math.floor(Math.random()*10.00)
-let weight = Math.floor(Math.random()*10.00)
+
+function randomWeight(){
+    let fishWeight = Math.floor(Math.random()*1000 / 100);
+    return fishWeight;
+}
+function randomValue(){
+    let fishValue = Math.floor(Math.random()*1000 / 100);
+    return fishValue;
+}
+
 
 function randomFish () {
     let Adj1 = fishAdj1[Math.floor(Math.random()*10)]
     let Adj2 = fishAdj2[Math.floor(Math.random()*10)]
     let newKind = fishKind[Math.floor(Math.random()*10)]
-
     let newFish = {
-     fishName :  Adj1 +  ' ' 
+     fishName :  + Adj1 +  ' ' 
      + Adj2  +  ' '
      + newKind +  ' '
-     + 'Value of ' + fishValue + '$  ' 
-     + 'Weight of '+ weight+ 'lbs',
-
      }
      return newFish
- }
-
-
-
-
-while (i < 6 ){
-    console.log("Nice! You caught a "(randomFish())
-
-
-//  if (toFish === 'k')  + i;
-
-
-
-  
-
-// // keep (add all dimensions to something)    or release fish. Generate new fish
-// else if (toFish === 'r')
-
     
-i++
+    }
+// if variable created inside a function, it needs to be called and then stored in another 
+//variable outside the function.  ie random value creates fish value, call random value and then pass
+//into a new variable
+ 
+while (hour < 7 && currentBucketWeight <= 10  ){
+randomFish();
+randomValue();
+randomWeight();
+let fishValue=randomValue();
+let fishWeight = randomWeight();
+//console.log(currentBucket)
+
+let to_Fish= prompt("Nice! You caught a " +  " This fish is worth " + fishValue + " and weighs " + fishWeight+ 'Choose: [k]eep or [r]elease this fish')
+console.log('Current time is ' + hour + ":00")
+
+    //let emptybucket = caught fishes
+
+  if (to_Fish === 'k'){
+currentBucket.push(randomFish);
+hour++;
+ 
+// increase weight
+
+  }
+// release fish
+else if (to_Fish === 'r'){
+    console.log("Fish on!")
+ 
 }
-
-
-
-
-// else {
-//     console.log("Error")
-// }
-
-
-
-
-
-
+else {
+    console.log('not a valid inuput')
+}
+} 
 // //  //k or r
 // // if (to_Fish === 'k')  {
 // //     // fish, add to inventory
@@ -68,33 +74,18 @@ i++
 // // }
 
 
-// // function addGrades (  student, newGrades){
-// //     for (let i=0 ; i <  newGrades.length; i++  )
-// //     student.grades.push(newGrades[i])
-// // // creating a function
+// // // function addGrades (  student, newGrades){
+// // //     for (let i=0 ; i <  newGrades.length; i++  )
+// // //     student.grades.push(newGrades[i])
+// // // // creating a function
 
 
-// // function newfish (fishKind, fishAdj1, fishAdj2) {
-// //     let randomFish = {}; 
-// //     for (let i = 0; i < fishKind.length; i++) {
+// // //     for (let i = 0; i < fishKind.length; i++) {
        
 
 
 
-
-
-// //     return randomFish;
-
-// // }
-
-
-
-
-
-
-
-
-
+// // // }
 
 
 
